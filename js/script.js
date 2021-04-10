03223f16f23b39b26f1f7da03d99de73a7694905
Caz Gabrielse <cgabrielse@outlook.com>
2021-04-09 23:58:35 -0500

    Handled job input field display case
    If the form user selects "other" in the job role select field, a text input field will display asking the user to describe their role.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 const nameInput = document.querySelector('#name');
-
 nameInput.focus();
+
+const jobInput = document.querySelector('#title');
+const otherJobInput = document.querySelector('#other-job-role');
+otherJobInput.style.display = 'none';
+
+jobInput.addEventListener('input', e => {
+    if(e.target.value === 'other')
+    {
+        otherJobInput.style.display = 'block';
+    }
+    else
+    {
+        otherJobInput.style.display = 'none';
+    }
+});
