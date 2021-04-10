19dc282297b430d5b572f4f8173f61bf96e62213
Caz Gabrielse <cgabrielse@outlook.com>
2021-04-10 13:42:11 -0500

    Added theme/color selection
    When a theme is selected, the form will display a select menu with related color options, and switch options if a different theme is selected.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Initialize focus to the "Name" input field
 const nameInput = document.querySelector('#name');
 nameInput.focus();
 
@@ -5,6 +6,7 @@ const jobInput = document.querySelector('#title');
 const otherJobInput = document.querySelector('#other-job-role');
 otherJobInput.style.display = 'none';
 
+// Hide/show "Other" job role input field based on selection
 jobInput.addEventListener('input', e => {
     if(e.target.value === 'other')
     {
@@ -13,5 +15,52 @@ jobInput.addEventListener('input', e => {
     else
     {
         otherJobInput.style.display = 'none';
+    }
+});
+
+const designInput = document.querySelector('#design');
+const colorInput = document.querySelector('#color');
+const colorDiv = colorInput.parentNode;
+const colors = colorInput.children;
+
+// Initialize color selection as hidden
+colorDiv.style.display = 'none';
+
+// When a theme is selected, display related color options
+designInput.addEventListener('input', e => {
+    
+    colorDiv.style.display = 'block';
+    
+    if(e.target.value === 'js puns')
+    {
+        colorInput.value = colors[0].value;
+
+        for(let i = 1; i < colors.length; i++)
+        {
+            if(colors[i].dataset.theme === 'js puns')
+            {
+                colors[i].style.display = 'block';
+            }
+            else if(colors[i].dataset.theme === 'heart js')
+            {
+                colors[i].style.display = 'none';
+            }
+        }
+    }
+    else if (e.target.value === 'heart js')
+    {
+        colorInput.value = colors[0].value;
+
+        for(let i = 1; i < colors.length; i++)
+        {
+            if(colors[i].dataset.theme === 'heart js')
+            {
+                colors[i].style.display = 'block';
+            }
+            else if(colors[i].dataset.theme === 'js puns')
+            {
+                colors[i].style.display = 'none';
+            }
+        }
     }
 });
