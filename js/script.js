c35c4b783e41dad21cc6430e3fd25bdf92a9159f
Caz Gabrielse <cgabrielse@outlook.com>
2021-04-10 23:06:05 -0500

    Added form validation
    Validates the following input fields: name, email, billing and if one or more activities are checked.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ otherJobInput.style.display = 'none';
 
 // Hide/show "Other" job role input field based on selection
 jobInput.addEventListener('input', e => {
+    
     if(e.target.value === 'other')
     {
         otherJobInput.style.display = 'block';
@@ -94,10 +95,10 @@ activities.addEventListener('input', e => {
 const paymentMethods = document.querySelectorAll('.payment-methods > div');
 const paymentSelect = document.querySelector('#payment');
 
-// Initialize payment method as 'Credit Card'
+// Initialize payment method select as 'Credit Card'
 paymentSelect.selectedIndex = '1';
 
-// Initialize other payment methods as hidden
+// Initialize other payment method divs as hidden
 for(let i = 2; i < paymentMethods.length; i++)
 {
     paymentMethods[i].style.display = 'none';
@@ -119,3 +120,88 @@ paymentSelect.addEventListener('input', e => {
         }
     }
 });
+
+/*** 
+ * Form Validation
+***/
+const form = document.querySelector('form');
+
+// Validate all form input on submit
+form.addEventListener('submit', e => {
+
+
+    // If any input is invalid, do not submit form
+    if(!validateName())
+    {
+        e.preventDefault();
+        console.log('Name is not valid.')
+    }
+    else if (!validateEmail())
+    {
+        e.preventDefault();
+        console.log('Email is not valid.')
+    }
+    else if (!validateBilling())
+    {
+        e.preventDefault();
+        console.log('Billing is not valid.')
+    }
+    else if(!validateActivity())
+    {
+        e.preventDefault();
+        console.log('Activity is not valid.')
+    }
+    else
+    {
+        // TODO: Submit form
+    }
+});
+
+// Validate that the name input field is not empty
+function validateName()
+{
+    const nameValue = nameInput.value;
+    const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
+    return nameIsValid;
+}
+
+// Validate that the email input field has the correct format
+function validateEmail()
+{
+    const emailValue = document.querySelector('#email').value;
+    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+    return emailIsValid;
+}
+
+// Validate credit card, zip code and cvv code are digits and correct length
+function validateBilling()
+{
+    if(paymentSelect.value === 'credit-card')
+    {
+        const numValue = document.querySelector('#cc-num').value;
+        const numIsValid = /^\d{4}\d{4}\d{4}\d{1,4}$/.test(numValue);
+
+        const zipValue = document.querySelector('#zip').value;
+        const zipIsValid = /^\d{5}$/.test(zipValue);
+
+        const cvvValue = document.querySelector('#cvv').value;
+        const cvvIsValid = /^\d{3}$/.test(cvvValue);
+    
+        return numIsValid && zipIsValid && cvvIsValid ? true : false;
+    }
+    else
+    {
+        // Do nothing
+    }
+}
+
+// Validate that at least one activity is checked
+function validateActivity()
+{
+    const activityInputs = document.querySelectorAll('#activities-box input');
+
+    for(let i = 0; i < activityInputs.length; i++)
+    {
+        return activityInputs[i].checked ? true : false;
+    }
+}
